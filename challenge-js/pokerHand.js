4393a2855ff7cc448d17e452b86f18c043de9a49
Kurt Miller <kmiller9393@gmail.com>
2018-12-07 14:34:52 -0700

    Add functionality that checks if user's hand is a flush

--- challenge-js/pokerHand.js
+++ challenge-js/pokerHand.js
@@ -1,9 +1,65 @@
 class PokerHand {
-  constructor() {}
+  constructor(cards) {
+    this.cards = cards;
+    this.suitTypes = {
+      heartCount: 0,
+      spadeCount: 0,
+      clubCount: 0,
+      diamondCount: 0
+    };
+    this.cardTypes = {
+      aceCount: 0,
+      kingCount: 0,
+      threeCount: 0
+    };
+  }
 
   getRank() {
-    // Implement poker hand ranking
-    return 'Royal Flush';
+    var userCards = this.cards.split(' ');
+
+    console.log(userCards);
+
+    if (userCards.length !== 5) return;
+
+    userCards.forEach(card => {
+      if (card.includes('A')) {
+        return this.cardTypes.aceCount++;
+      }
+    });
+
+    userCards.forEach(card => {
+      if (card.includes('K')) {
+        return this.cardTypes.kingCount++;
+      }
+    });
+
+    userCards.forEach(card => {
+      if (card.includes('3')) {
+        return this.cardTypes.threeCount++;
+      }
+    });
+
+    userCards.forEach(card => {
+      if (card.includes('h')) {
+        return this.suitTypes.heartCount++;
+      }
+    });
+
+    if (this.cardTypes.aceCount === 2) {
+      return 'One Pair';
+    }
+
+    if (this.cardTypes.aceCount === 3) {
+      return 'Three of A Kind';
+    }
+
+    if (this.cardTypes.kingCount === 2 && this.cardTypes.threeCount === 2) {
+      return 'Two Pair';
+    }
+
+    if (this.suitTypes.heartCount === 5) {
+      return 'Flush';
+    }
   }
 }
 
