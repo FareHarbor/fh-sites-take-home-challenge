1b335b954dd88ce2d795d74894bf88e4f8f2d9fd
Johnny Garcia <johnnyg2015@gmail.com>
2020-04-15 13:44:11 -0600

    refactor consolidation methods

--- challenge-php/src/PokerHand/PokerHand.php
+++ challenge-php/src/PokerHand/PokerHand.php
@@ -3,8 +3,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-
-
 class PokerHand
 {
   public $cardsString;
@@ -12,10 +10,10 @@ class PokerHand
 
   //stores how many cards of each suit are in the hand
   public static $suitsArray = array(
-        "diamonds" => 0,
-        "hearts" => 0,
-        "clubs" => 0,
-        "spades" => 0,
+        "d" => 0,
+        "h" => 0,
+        "c" => 0,
+        "s" => 0,
   );
 
   //stores how many cards of each value are in the hand
@@ -48,16 +46,8 @@ class PokerHand
         self::$cardsArray[] = new Card($this->cardsString[3]);
         self::$cardsArray[] = new Card($this->cardsString[4]);
 
-
-//sorts hand by value
-        usort(self::$cardsArray, array($this, "compareValues"));
-
-
         $this->consolidateSuits();
         $this->consolidateValues();
-
-        //echo print_r(self::$valuesArray) . "<br>";
-        //echo print_r(self::$suitsArray) . "<br>";
 
 //prints cards
         $this->printCards();
@@ -91,9 +81,7 @@ class PokerHand
           }
       }
 
-//tests for each possible hand.
-
-
+//methods to test for each possible hand.
       public function testRoyalFlush(){
           return ($this->testStraight(10) && $this->testFlush());
       }
@@ -119,7 +107,6 @@ class PokerHand
         return false;
       }
 
-
       public function testStraight($start = 2){
         for ($i=$start; $i<=10; $i++){
           if (self::$valuesArray[$i] == 1 && self::$valuesArray[$i + 1] == 1 && self::$valuesArray[$i + 2] == 1 && self::$valuesArray[$i + 3] == 1 && self::$valuesArray[$i + 4] == 1){
@@ -132,7 +119,6 @@ class PokerHand
       public function test3oak(){
         return (array_search(3, self::$valuesArray) && array_search(2, self::$valuesArray) == 0);
       }
-
 
       public function test2pair(){
         $temp = array_count_values(self::$valuesArray);
@@ -148,94 +134,18 @@ class PokerHand
       }
       }
 
-//iteratres through cards and counts how many of each suit there are, stores in suitsArray
+//iterates through cards and counts how many of each suit there are, stores in suitsArray
       public function consolidateSuits(){
         foreach (self::$cardsArray as $card){
-          switch (strtolower($card->getSuit())){
-            case 'h' :
-              self::$suitsArray["hearts"]++;
-              break;
-            case 's' :
-              self::$suitsArray["spades"]++;
-              break;
-            case 'c' :
-              self::$suitsArray["clubs"]++;
-              break;
-            case 'd' :
-              self::$suitsArray["diamonds"]++;
-              break;
-          }
+          self::$suitsArray[strtolower($card->getSuit())]++;
         }
       }
-
 //iterates through cards and counts how many of each value there are , stores in valuesarray
       public function consolidateValues(){
             foreach (self::$cardsArray as $card){
-              switch($card->getValue()){
-                case 2 :
-                  self::$valuesArray["2"]++;
-                  break;
-                case 3 :
-                  self::$valuesArray["3"]++;
-                  break;
-                case 4 :
-                  self::$valuesArray["4"]++;
-                  break;
-                case 5 :
-                  self::$valuesArray["5"]++;
-                  break;
-                case 6 :
-                  self::$valuesArray["6"]++;
-                  break;
-                case 7 :
-                  self::$valuesArray["7"]++;
-                  break;
-                case 8 :
-                  self::$valuesArray["8"]++;
-                  break;
-                case 9 :
-                  self::$valuesArray["9"]++;
-                  break;
-                case 10 :
-                  self::$valuesArray["10"]++;
-                  break;
-                case 11 :
-                  self::$valuesArray["11"]++;
-                  break;
-                case 12 :
-                  self::$valuesArray["12"]++;
-                  break;
-                case 13 :
-                  self::$valuesArray["13"]++;
-                  break;
-                case 14 :
-                  self::$valuesArray["14"]++;
-                  break;
-              }
-
-
+              self::$valuesArray[$card->getValue()]++;
             }
-
       }
-
-//compares two card objects by their value
-      public function compareValues(Card $a, Card $b)
-      {
-          if ($a->getValue() == $b->getValue()) {
-              return 0;
-            }
-          return ($a->getValue() < $b->getValue()) ? -1 : 1;
-      }
-
-//compares two card objects by their suit
-      public function compareSuits(Card $a, Card $b){
-        if ($a->getSuit() == $b->getSuit()){
-          return 1;
-        } else {
-          return 0;
-        }
-      }
-
 
 //prints cards in hand
       public function printCards(){
@@ -243,7 +153,6 @@ class PokerHand
             echo $card->getValue() . " of " . $card->getSuit() . " </br> ";
         }
       }
-
 }
 
 //class for each card, contains a value and suit property
@@ -296,11 +205,8 @@ public $suit;
 public function getCard(){
   return $this->value . " of " . $this->suit . "</br>";
 }
-
-
-
 }
 
-$myHand = new PokerHand('9s 9S KS QS js');
+$myHand = new PokerHand('9s 9S jS Qs js');
 
 ?>
